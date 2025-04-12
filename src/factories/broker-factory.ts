7a1f63ca808a9eda0b26f4fb66ef7b8fcc922743
Raj Yadav <raj819314@gmail.com>
2025-04-13 02:25:54 +0530

    initial commit

--- src/factories/broker-factory.ts
+++ src/factories/broker-factory.ts
@@ -8,7 +8,10 @@ export const createMessageBroker = (): MessageBroker => {
   console.log("connecting to kafka broker...");
   // singleton
   if (!broker) {
-    broker = new KafkaBroker("<clientid>", [config.get("kafka.broker")]);
+    broker = new KafkaBroker("notification-service", [
+      // This is basically url on which kafka server is running
+      config.get<string>("kafka.broker"),
+    ]);
   }
   return broker;
 };
